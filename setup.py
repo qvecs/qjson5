b2abfb173c42cd42cd8df296d28c5c8c668f4e8d
synchronizing <2829082+synchronizing@users.noreply.github.com>
2025-05-18 17:59:40 -0400

    Updated to v1.0.3.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="qjson5",
-    version="1.0.2",
+    version="1.0.3",
     author="Quick Vectors",
     author_email="felipe@qvecs.com",
     description="JSON5 parser for Python written in C.",
